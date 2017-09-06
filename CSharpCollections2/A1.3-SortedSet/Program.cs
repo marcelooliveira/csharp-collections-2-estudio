49261fc6b5fd7dc0b5065317ea11ea81e9649812
alexfelipe <alexfelipevieira@gmail.com>
2017-09-06 15:39:51 -0300

    .
    .

---
+++ CSharpCollections2/A1.3-SortedSet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1._3_SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conjunto de alunos:
+            //"Vanessa Tonini",
+            //"Ana Losnak",
+            //"Rafael Nercessian",
+            //"Priscila Stuani"
+
+
+            //adicionar: Rafael Rollo
+            //adicionar: Fabio Gushiken
+            //adicionar: Fabio Gushiken
+            //adicionar: FABIO GUSHIKEN
+
+            //este conjunto é subconjunto do outro? IsSubsetOf
+
+
+            //este conjunto é superconjunto do outro? IsSupersetOf
+
+            //os conjuntos contêm os mesmos elementos? SetEquals
+
+            //subtrai os elementos da outra coleção que também estão neste conjunto - ExceptWith
+
+            //intersecção dos conjuntos - IntersectWith
+            
+            //somente em um ou outro conjunto - SymmetricExceptWith
+
+            //união de conjuntos - UnionWith
+
+        }
+    }
+}
