13231d6f2c94e8c9329e6c5f699d3f9044a24e8a
alexfelipe <alexfelipevieira@gmail.com>
2017-09-06 14:03:25 -0300

    652-csharpcollections2-video1.2-SortedList
    652-csharpcollections2-video1.2-SortedList

--- CSharpCollections2/A1.1-SortedList/Program.cs
+++ CSharpCollections2/A1.1-SortedList/Program.cs
@@ -10,20 +10,53 @@ namespace A1._1_SortedList
     {
         static void Main(string[] args)
         {
-            ///vamos criar um dicionário de alunos
-            ///VT, Vanessa, 34672
-            ///AL, Ana, 5617
-            ///RN, Rafael, 17645
-            ///WM, Wanderson, 11287
+            //vamos criar um dicionário de alunos
+            //VT, Vanessa, 34672
+            //AL, Ana, 5617
+            //RN, Rafael, 17645
+            //WM, Wanderson, 11287
+            IDictionary<string, Aluno> alunos
+                = new Dictionary<string, Aluno>();
 
-            ///imprimindo...
+            alunos.Add("VT", new Aluno("Vanessa", 34672));
+            alunos.Add("AL", new Aluno("Ana", 5617));
+            alunos.Add("RN", new Aluno("Rafael", 17645));
+            alunos.Add("WM", new Aluno("Wanderson", 11287));
 
-            ///vamos remover: AL
-            ///vamos adicionar: MO
+            //imprimindo...
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
 
-            ///como é internamente um Dictionary?
+            //vamos remover: AL
+            alunos.Remove("AL");
+            //vamos adicionar: MO
+            alunos.Add("MO", new Aluno("Marcelo", 12345));
+            Console.WriteLine();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
 
-            ///apresentando nova coleção...SortedList
+            //como é internamente um Dictionary?
+            ///<image url="$(ProjectDir)\Slides\img1.png" scale=""/>
+
+            //apresentando nova coleção...SortedList
+
+            IDictionary<string, Aluno> sorted
+                = new SortedList<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            Console.WriteLine();
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
