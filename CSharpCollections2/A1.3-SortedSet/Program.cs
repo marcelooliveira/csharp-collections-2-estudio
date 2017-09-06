a1d9b883d58974a336b6d90f4f2dc9e68396c3eb
alexfelipe <alexfelipevieira@gmail.com>
2017-09-06 15:54:27 -0300

    652-csharpcollections2-video1.4-SortedSet.trec
    652-csharpcollections2-video1.4-SortedSet.trec

--- CSharpCollections2/A1.3-SortedSet/Program.cs
+++ CSharpCollections2/A1.3-SortedSet/Program.cs
@@ -11,31 +11,43 @@ namespace A1._3_SortedSet
         static void Main(string[] args)
         {
             //Conjunto de alunos:
-            //"Vanessa Tonini",
-            //"Ana Losnak",
-            //"Rafael Nercessian",
-            //"Priscila Stuani"
-
+            ISet<string> alunos =
+                new SortedSet<string>()
+                {
+                    "Vanessa Tonini",
+                    "Ana Losnak",
+                    "Rafael Nercessian",
+                    "Priscila Stuani"
+                };
 
             //adicionar: Rafael Rollo
+            alunos.Add("Rafael Rollo");
             //adicionar: Fabio Gushiken
+            alunos.Add("Fabio Gushiken");
             //adicionar: Fabio Gushiken
+            alunos.Add("Fabio Gushiken");
             //adicionar: FABIO GUSHIKEN
+            alunos.Add("FABIO GUSHIKEN");
 
-            //este conjunto é subconjunto do outro? IsSubsetOf
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            ///este conjunto é subconjunto do outro? IsSubsetOf
 
 
-            //este conjunto é superconjunto do outro? IsSupersetOf
+            ///este conjunto é superconjunto do outro? IsSupersetOf
 
-            //os conjuntos contêm os mesmos elementos? SetEquals
+            ///os conjuntos contêm os mesmos elementos? SetEquals
 
-            //subtrai os elementos da outra coleção que também estão neste conjunto - ExceptWith
+            ///subtrai os elementos da outra coleção que também estão neste conjunto - ExceptWith
 
-            //intersecção dos conjuntos - IntersectWith
-            
-            //somente em um ou outro conjunto - SymmetricExceptWith
+            ///intersecção dos conjuntos - IntersectWith
 
-            //união de conjuntos - UnionWith
+            ///somente em um ou outro conjunto - SymmetricExceptWith
+
+            ///união de conjuntos - UnionWith
 
         }
     }
