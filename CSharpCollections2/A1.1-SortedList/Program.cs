6bc6c2245ddd8778b3bf2e0d6da5d5ba89e83b92
alexfelipe <alexfelipevieira@gmail.com>
2017-09-06 13:01:26 -0300

    .
    .

---
+++ CSharpCollections2/A1.1-SortedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1._1_SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///vamos criar um dicionário de alunos
+            ///VT, Vanessa, 34672
+            ///AL, Ana, 5617
+            ///RN, Rafael, 17645
+            ///WM, Wanderson, 11287
+
+            ///imprimindo...
+
+            ///vamos remover: AL
+            ///vamos adicionar: MO
+
+            ///como é internamente um Dictionary?
+
+            ///apresentando nova coleção...SortedList
+        }
+    }
+}
