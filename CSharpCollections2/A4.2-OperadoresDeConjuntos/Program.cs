b86a023269e23f6d39841c707e67a5072aefacb9
alexfelipe <alexfelipevieira@gmail.com>
2017-09-12 16:11:07 -0300

    .
    .

---
+++ CSharpCollections2/A4.2-OperadoresDeConjuntos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4._2_OperadoresDeConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seq1 = { "janeiro", "fevereiro", "março" };
+            string[] seq2 = { "fevereiro", "MARÇO", "abril" };
+            
+
+        }
+    }
+}
